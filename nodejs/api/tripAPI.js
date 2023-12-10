10b947f3f09bb427498ae15048b9185744c4aa08
millie0808 <102743839+millie0808@users.noreply.github.com>
2023-12-10 19:31:51 +0800

    feat: get personal trips and public trips

--- nodejs/api/tripAPI.js
+++ nodejs/api/tripAPI.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const tripAPI = express.Router();
 
+const cityController = require('../controllers/cityController');
 const tripController = require('../controllers/tripController');
 const scheduleController = require('../controllers/scheduleController');
 
 // Create new trip
 tripAPI.post('/api/trip', async (req, res) => {
     try{
-        const newTripJSON = req.body;
-        const newTripData = await tripController.mapToTripData(newTripJSON);
-        const newTrip = await tripController.createTrip(newTripData);
-        const newScheduleDataList = await scheduleController.mapToScheduleData(newTripJSON, newTrip.id);
-        await scheduleController.createSchedule(newScheduleDataList);
-        // 成功的回應
+        const newTrip = req.body;
+        await tripController.checkNewTrip(newTrip);
+        const cityId = await cityController.findId(newTrip.city);
+        const tripNumber = await tripController.createTrip(cityId, newTrip.date, newTrip.city, newTrip.userId, newTrip.name);
         res.status(201).json({
             ok: true,
-            trip: newTrip
+            trip: {
+                number: tripNumber
+            }
         });
     }
     catch(error){
@@ -39,10 +40,36 @@ tripAPI.get('/api/trip/', async (req, res) => {
     try{
         const tripNumber = req.query.number;
         const trip = await tripController.getTrip(tripNumber);
+        const schedule = await scheduleController.getSchedule(trip);
         res.status(200).json({
             ok: true,
-            trip: trip
+            trip: trip,
+            schedule: schedule
         });
+    }
+    catch(error){
+        res.status(500).json({
+            error: 'Internal Server Error'
+        });
+    }
+})
+
+tripAPI.get('/api/trips', async (req, res) => {
+    try{
+        const userId = req.query.userId;
+        if(userId){
+            const trips = await tripController.getTripsByUserId(userId);
+            res.status(200).json({
+                ok: true,
+                trips: trips
+            });
+        } else {
+            const publicTrips = await tripController.getPublicTrips();
+            res.status(200).json({
+                ok: true,
+                trips: publicTrips
+            });
+        }
     }
     catch(error){
         res.status(500).json({
