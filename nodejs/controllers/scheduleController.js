23ccaffae941897e94a0d1672dde8a57ebf8f72d
millie0808 <102743839+millie0808@users.noreply.github.com>
2023-12-10 19:47:19 +0800

    refactor: schedule CRUD

--- nodejs/controllers/scheduleController.js
+++ nodejs/controllers/scheduleController.js
@@ -1,11 +1,22 @@
 const Schedule = require('../models/scheduleModel');
-
+const Site = require('../models/siteModel');
 class ScheduleController {
-    async createSchedule(scheduleDataList){
+    async createSchedule(siteIds, schedules, durations, tripId){
         try{
-            scheduleDataList.forEach( async (scheduleData) => {
-                const newSchedule = await Schedule.create(scheduleData);
-            });
+            let i = 0;
+            for (const day in schedules){
+                for(let j = 0; j < schedules[day].length; j++){
+                    await Schedule.create({
+                        trip_id: tripId,
+                        site_id: siteIds[i],
+                        day_order: day,
+                        site_order: j,
+                        mode: durations[day][j]?.mode || null,
+                        duration: durations[day][j]?.duration || null,
+                    })
+                    i = i + 1;
+                }
+            }
         }
         catch(error){
             console.error('Error creating schedule:', error);
@@ -13,16 +24,35 @@ class ScheduleController {
         }
     }
 
-    // async getSchedule() {
-    //     try {
-    //         const trips = await Schedule.findAll();
-    //         return trips;
-    //     } 
-    //     catch (error) {
-    //         console.error('Error retrieving schedule:', error);
-    //         throw error;
-    //     }
-    // }
+    async getSchedule(trip) {
+        try {
+            const schedule = await Schedule.findAll({
+                where: {trip_id: trip.id},
+                attributes: { exclude: ['trip_id', 'site_id'] },
+                include: {
+                    model: Site,
+                    attributes: { exclude: ['id'] },
+                    as: 'site',
+                    order: [['day_order', 'ASC'], ['site_order', 'ASC']],
+                },
+                raw: true,
+            });
+            const numberOfDays = countNumberOfDays(trip.start_date, trip.end_date);
+            let scheduleData = {};
+            for(let i = 0; i < numberOfDays; i++){
+                scheduleData[i] = schedule.filter(item => item.day_order === i);;
+            }
+            return scheduleData
+        } 
+        catch (error) {
+            console.error('Error retrieving schedule:', error);
+            throw error;
+        }
+    }
+
+    async deleteSchedule(tripId){
+        Schedule.destroy({ where: { trip_id: tripId } });
+    }
 
     async mapToScheduleData(newTripJSON, tripId){
         try{
@@ -55,3 +85,14 @@ class ScheduleController {
 const scheduleController = new ScheduleController();
 
 module.exports = scheduleController;
+
+function countNumberOfDays(start, end){
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+
+    const timeDifference = endDate.getTime() - startDate.getTime();
+    const daysDifference = timeDifference / (1000 * 3600 * 24);
+
+    const days = Math.round(daysDifference) + 1;
+    return days
+}
